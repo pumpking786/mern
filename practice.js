a4af8d8b4acc90e2bdbe69ad9b07f8838601decf
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-05-15 16:06:45 +0545

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
 
+/////////////////13//////////////////
+
+// const ram=async ()=>{
+//     // return //resolve
+//     // throw //reject
+// }
+
+///////////////14///////////////
+
+// class User{
+//     name;
+//     address;
+//     price;
+
+//     getName(){
+//         return this.name
+//     }
+//     setName=function (_name){
+//         this.name=_name
+//     }
+//     setAddress=(_address)=>{
+//         this.address=_address
+//     }
+// }
+
+//////////////15/////////////////
