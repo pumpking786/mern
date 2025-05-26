01b2d594461cdbfb5df3602485beb7045614a810
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-05-26 17:33:18 +0545

    Update practice.js

--- practice.js
+++ practice.js
@@ -25,3 +25,55 @@
 // }
 
 //////////////15/////////////////
+
+/////////////////////////15/////////////////////////
+class Man{
+    name;
+    class;
+    dob;
+    age;
+
+    constructor(_name,_class,_dob){
+    this.name=_name
+    this.class=_class
+    this.dob=_dob
+    this.getAge()
+}
+
+getAge=()=>{
+    let nowdate=new Date()
+    // console.log(nowdate);
+    this.dob=new Date(this.dob)
+    let age=Math.ceil((nowdate-this.dob)/(1000 * 60 * 60 * 24 * 365.25))
+    this.age=age
+    console.log(age);
+
+}
+
+}
+const user_obj=new Man("Sam","2","1991-07-01")
+
+///////////////////BREAK////////////////////
+////////////////INHERITANCE/////////////////
+
+class Person{
+    name;
+    age;
+    class;
+    role;
+
+    login=()=>{
+
+    }
+}
+
+class Customer extends Person{
+    role='customer';
+}
+
+const customer_obj=new Customer()
+
+class Admin extends Person{
+    role='admin';
+}
+const admin_obj=new Admin()
