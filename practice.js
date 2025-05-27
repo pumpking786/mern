85850ecc7fbae99ee0f2ca65196d5f30dca404e4
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-05-27 17:09:50 +0545

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,79 +1,22 @@
-
-/////////////////13//////////////////
-
-// const ram=async ()=>{
-//     // return //resolve
-//     // throw //reject
+/////////////16//////////
+// interface Name{
+//     getName()
 // }
-
-///////////////14///////////////
-
-// class User{
-//     name;
-//     address;
-//     price;
-
+// trait Name{
 //     getName(){
 //         return this.name
 //     }
-//     setName=function (_name){
-//         this.name=_name
-//     }
-//     setAddress=(_address)=>{
-//         this.address=_address
-//     }
 // }
-
-//////////////15/////////////////
-
-/////////////////////////15/////////////////////////
-class Man{
-    name;
-    class;
-    dob;
-    age;
-
-    constructor(_name,_class,_dob){
-    this.name=_name
-    this.class=_class
-    this.dob=_dob
-    this.getAge()
-}
-
-getAge=()=>{
-    let nowdate=new Date()
-    // console.log(nowdate);
-    this.dob=new Date(this.dob)
-    let age=Math.ceil((nowdate-this.dob)/(1000 * 60 * 60 * 24 * 365.25))
-    this.age=age
-    console.log(age);
-
-}
-
-}
-const user_obj=new Man("Sam","2","1991-07-01")
-
-///////////////////BREAK////////////////////
-////////////////INHERITANCE/////////////////
-
-class Person{
-    name;
-    age;
-    class;
-    role;
-
-    login=()=>{
-
+class User{
+    constructor(){
+        console.log("User");
     }
 }
 
-class Customer extends Person{
-    role='customer';
-}
-
-const customer_obj=new Customer()
-
-class Admin extends Person{
-    role='admin';
+class Admin extends User{
+    constructor(){
+        super()
+        console.log("Admin");
+    }
 }
 const admin_obj=new Admin()
